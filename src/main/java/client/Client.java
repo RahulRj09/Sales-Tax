09e729b0d7ca40c29f1d370c7ca685b4a8764b76
RahulRj09 <rahul18@navgurukul.org>
2019-08-18 16:56:11 +0530

    add . variable category

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,4 +1,10 @@
 package client;
 
+import salestax.Product;
+
 public class Client {
+    public static void main(String[] args) {
+        Product product = new Product("book","java",20,1);
+        System.out.println(product.name);
+    }
 }
--- src/main/java/salestax/Product.java
+++ src/main/java/salestax/Product.java
@@ -1,11 +1,13 @@
 package salestax;
 
 public class Product {
-    String name;
+    String category;
+    public String name;
     double price;
     int quontity;
 
-    public Product(String name, double price, int quontity) {
+    public Product(String category,String name, double price, int quontity) {
+        this.category = category;
         this.name = name;
         this.price = price;
         this.quontity = quontity;
