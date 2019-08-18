5576d66fc63544855a2affcde0f5ad428b29cd1d
RahulRj09 <rahul18@navgurukul.org>
2019-08-18 16:38:59 +0530

    add . constructor for product class

--- src/main/java/salestax/Product.java
+++ src/main/java/salestax/Product.java
@@ -4,4 +4,10 @@ public class Product {
     String name;
     double price;
     int quontity;
+
+    public Product(String name, double price, int quontity) {
+        this.name = name;
+        this.price = price;
+        this.quontity = quontity;
+    }
 }
