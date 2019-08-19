54eb7b4f53d30ccacdc0fe91e15170c1e6d4ba87
RahulRj09 <rahul18@navgurukul.org>
2019-08-19 12:27:00 +0530

    add . class product factory

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -4,7 +4,8 @@ import salestax.Product;
 
 public class Client {
     public static void main(String[] args) {
-        Product product = new Product("book","java",20,1);
+        Product product = new Product("book","java",20,2);
         System.out.println(product.name);
+        System.out.println(product.getPrice());
     }
 }
--- src/main/java/salestax/Product.java
+++ src/main/java/salestax/Product.java
@@ -1,15 +1,19 @@
 package salestax;
 
 public class Product {
-    String category;
+    private String category;
     public String name;
-    double price;
-    int quontity;
+    private double price;
+    private int quontity;
 
     public Product(String category,String name, double price, int quontity) {
         this.category = category;
         this.name = name;
         this.price = price;
         this.quontity = quontity;
+    }
+
+    public double getPrice() {
+        return price * quontity;
     }
 }
