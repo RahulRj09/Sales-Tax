a3b4666926d4a0dec21bef74ed8c1e184717cf4c
RahulRj09 <rahul18@navgurukul.org>
2019-08-21 18:43:39 +0530

    add. test case for add cart

--- src/test/java/salestaxtest/CartTest.java
+++ src/test/java/salestaxtest/CartTest.java
@@ -1,6 +1,21 @@
 package salestaxtest;
+import org.junit.Test;
+import salestax.Cart;
+import salestax.Product;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
 public class CartTest {
-
+@Test
+    public  void isShouldBeAddTheProductInCart(){
+    Map<Product,Integer> expectedProducts = new HashMap<>();
+    Product product = new Product("Book","java",100.0,false);
+    Cart cart = new Cart();
+    cart.add(product,1);
+    expectedProducts.put(product,1);
+    assertEquals(expectedProducts,cart.getProducts());
+}
 
 }
