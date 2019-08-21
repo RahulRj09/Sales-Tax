d85144182ef2f217ea8a06d17b1348ad9ef2bb2f
RahulRj09 <rahul18@navgurukul.org>
2019-08-21 18:50:22 +0530

    add. TaxCalculatorTest class

--- src/test/java/salestaxtest/CartTest.java
+++ src/test/java/salestaxtest/CartTest.java
@@ -1,4 +1,5 @@
 package salestaxtest;
+
 import org.junit.Test;
 import salestax.Cart;
 import salestax.Product;
@@ -7,15 +8,16 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
+
 public class CartTest {
-@Test
-    public  void isShouldBeAddTheProductInCart(){
-    Map<Product,Integer> expectedProducts = new HashMap<>();
-    Product product = new Product("Book","java",100.0,false);
-    Cart cart = new Cart();
-    cart.add(product,1);
-    expectedProducts.put(product,1);
-    assertEquals(expectedProducts,cart.getProducts());
-}
+    @Test
+    public void isShouldBeAddTheProductInCart() {
+        Map<Product, Integer> expectedProducts = new HashMap<>();
+        Product product = new Product("Book", "java", 100.0, false);
+        Cart cart = new Cart();
+        cart.add(product, 1);
+        expectedProducts.put(product, 1);
+        assertEquals(expectedProducts, cart.getProducts());
+    }
 
 }
