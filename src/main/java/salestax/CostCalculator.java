0888e3c975ab39a8d12fcdbb34e9cae31f4f6415
RahulRj09 <rahul18@navgurukul.org>
2019-08-22 09:59:20 +0530

    add. cost calculate with tax using costCalculateWithTax

--- src/main/java/salestax/CostCalculator.java
+++ src/main/java/salestax/CostCalculator.java
@@ -3,19 +3,22 @@ package salestax;
 import java.util.Map;
 
 public class CostCalculator {
-    public Map<Product,Integer> costCalculate(Cart cart) {
+    public Map<Product, Integer> costCalculate(Cart cart) {
         Map<Product, Integer> products = cart.getProducts();
-        for (Map.Entry<Product,Integer> product: products.entrySet()) {
+        for (Map.Entry<Product, Integer> product : products.entrySet()) {
             product.getKey().price = getPrice(product);
         }
         return products;
     }
 
     private double getPrice(Map.Entry<Product, Integer> product) {
-       return product.getKey().price* product.getValue();
+        return product.getKey().price * product.getValue();
     }
 
-    public Map<Product,Integer> costCalculateWithTax(Map<Product, Double> products){
-        return null;
+    public Map<Product, Double> costCalculateWithTax(Map<Product, Double> products) {
+        for (Map.Entry<Product, Double> product : products.entrySet()) {
+            product.getKey().price += product.getValue();
+        }
+        return products;
     }
 }
