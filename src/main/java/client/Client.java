a716f07ef4deaf7b0064c5b12c95639511208973
RahulRj09 <rahul18@navgurukul.org>
2019-08-22 17:25:18 +0530

    add. getTaxAmount method

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -9,10 +9,12 @@ public class Client {
     public static void main(String[] args) {
         Product product = new Product("book", "java", 12, false);
         Product product1 = new Product("book", "java", 12, true);
+        Product product2 = new Product("music", "CD", 14.99, true);
 
         Cart cart = new Cart();
         cart.add(product, 4);
         cart.add(product1, 2);
+        cart.add(product2,1);
 
         TaxCalculator taxCalculator = new TaxCalculator();
         Map<Product, Double> productsTax = taxCalculator.getTaxOfMap(cart);
--- src/main/java/salestax/TaxCalculator.java
+++ src/main/java/salestax/TaxCalculator.java
@@ -13,25 +13,27 @@ public class TaxCalculator {
     public Map<Product, Double> getTaxOfMap(Cart cart) {
         Map<Product, Integer> products = cart.getProducts();
         for (Map.Entry<Product, Integer> product : products.entrySet()) {
-            double taxAmount = 0;
             Product key = product.getKey();
             double price = key.getPrice() * product.getValue();
-            if (key.isImported()) {
-                taxAmount += getTax(price, IMPORT_DUTY);
-            }
-            if (!isTaxable(key)) {
-                taxAmount += getTax(price, BASIC_TAX_RATE);
-            }
+            double taxAmount = getTaxAmount(key, price);
             productsTax.put(key, taxAmount);
         }
         return productsTax;
+    }
+
+    private double getTaxAmount(Product key, double price) {
+        double taxAmount = 0;
+        if (key.isImported()) {
+            taxAmount += price * IMPORT_DUTY;
+        }
+        if (!isTaxable(key)) {
+            taxAmount += price * BASIC_TAX_RATE;
+        }
+        return taxAmount;
     }
 
     private boolean isTaxable(Product key) {
         return exemptCategory.contains(key.getCategory().toUpperCase());
     }
 
-    private double getTax(double price, double taxRate) {
-        return price * taxRate;
-    }
 }
