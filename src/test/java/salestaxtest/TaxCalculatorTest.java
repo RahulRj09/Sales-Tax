dced79155e9a49417cd003e6b96c449544f5eec3
RahulRj09 <rahul18@navgurukul.org>
2019-08-22 10:34:52 +0530

    fix. test case for importedTaxAmount

--- src/test/java/salestaxtest/TaxCalculatorTest.java
+++ src/test/java/salestaxtest/TaxCalculatorTest.java
@@ -12,32 +12,13 @@ public class TaxCalculatorTest {
     @Test
     public void itShouldBeGiveTheImportedTaxAmount(){
         Map<Product, Double> expectedProductsWithTax = new HashMap<>();
-        Product product = new Product("Book", "java", 100.0, true);
+        Product product = new Product("Book", "java", 200.0, true);
         Cart cart = new Cart();
-        cart.add(product, 1);
-        expectedProductsWithTax.put(product,5.0);
-        TaxCalculator taxCalculator = new TaxCalculator();
-        assertEquals(expectedProductsWithTax,taxCalculator.getTax(cart));
-    }
-    @Test
-    public void itShouldBeGiveTheBasicTaxAmount(){
-        Map<Product, Double> expectedProductsWithTax = new HashMap<>();
-        Product product = new Product("music", "cd", 100.0, false);
-        Cart cart = new Cart();
-        cart.add(product, 1);
+        cart.add(product, 2);
         expectedProductsWithTax.put(product,10.0);
         TaxCalculator taxCalculator = new TaxCalculator();
-        assertEquals(expectedProductsWithTax,taxCalculator.getTax(cart));
+        assertEquals(expectedProductsWithTax,taxCalculator.getTax(cart.getProducts()));
     }
-    @Test
-    public void itShouldBeGiveTheBasicTaxAmountAndImportedTaxAmount(){
-        Map<Product, Double> expectedProductsWithTax = new HashMap<>();
-        Product product = new Product("music", "cd", 100.0, true);
-        Cart cart = new Cart();
-        cart.add(product, 1);
-        expectedProductsWithTax.put(product,15.0);
-        TaxCalculator taxCalculator = new TaxCalculator();
-        assertEquals(expectedProductsWithTax,taxCalculator.getTax(cart));
-    }
+
 
 }
