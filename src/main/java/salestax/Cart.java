c22641f00d09d7b28ee49bd941ea39a2a91b7a32
RahulRj09 <rahul18@navgurukul.org>
2019-08-25 22:50:19 +0530

    refactor

--- src/main/java/salestax/Cart.java
+++ src/main/java/salestax/Cart.java
@@ -1,25 +1,19 @@
 package salestax;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
 
 public class Cart {
 
-    private Map<Product, Integer> products = new HashMap<>();
     private List<CartItem> cartItems = new ArrayList<>();
 
-    public Map<Product, Integer> getProducts() {
-        return products;
-    }
 
     public List<CartItem> getCartItems() {
         return cartItems;
     }
 
     public void add(Product product, Integer quantity) {
-        products.put(product, quantity);
         CartItem cartItem = new CartItem(product, quantity);
         cartItems.add(cartItem);
     }
